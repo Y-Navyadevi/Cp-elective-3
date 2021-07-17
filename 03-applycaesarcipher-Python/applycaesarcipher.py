9da1209081e10ce0783c7a36896dd0806f7aae8d
Y-Navyadevi <navyadevi789@msitprogram.net>
2021-07-17 11:32:47 +0530

    P3D4

--- 03-applycaesarcipher-Python/applycaesarcipher.py
+++ 03-applycaesarcipher-Python/applycaesarcipher.py
@@ -11,8 +11,21 @@
 
 
 def fun_applycaesarcipher(msg, shift):
-	return ""
-
+    result = ""
+ 
+    # traverse text
+    for i in range(len(msg)):
+        char = msg[i]
+ 
+        # Encrypt uppercase characters
+        if (char.isupper()):
+            result += chr((ord(char) + shift-65) % 26 + 65)
+ 
+        # Encrypt lowercase characters
+        else:
+            result += chr((ord(char) + shift - 97) % 26 + 97)
+ 
+    return result
 
 
 
