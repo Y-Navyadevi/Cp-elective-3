f07f1b6bcc1bdad938f23b93a3fc1746fef81b6a
Y-Navyadevi <navyadevi789@msitprogram.net>
2021-07-27 12:38:53 +0530

    P1D11

--- 10-issorted-Python/issorted.py
+++ 10-issorted-Python/issorted.py
@@ -4,7 +4,15 @@
 # must only consider each value in the list once (so, in terms of big-oh, which we will 
 # learn soon, it runs in O(n) time, where n=len(a)), and so in particular you may not sort 
 # the list.
-
 def issorted(a):
-	# your code goes here
-	pass
+    c=0
+    for i in range(len(a)):
+        if(i==len(a)-1):
+            break
+        if(a[i]>a[i+1]):
+            c+=1
+    if(c==0):
+        return True
+    elif(c==len(a)-1):
+        return True
+    return False
